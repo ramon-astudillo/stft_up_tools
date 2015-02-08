e6f35b0894129e345d6d3cb898f3bdea1af4e962
Ramón Astudillo <ramon@astudillo.com>
2015-02-08 20:35:52 +0000

    Removed special cases for zero amplitude input

--- speech_enhancement/comp_mmse.m
+++ speech_enhancement/comp_mmse.m
@@ -6,46 +6,31 @@ switch method
     case 'Wiener'
         
         % MMSE estimate is directly in complex domain
-        hat_X                 = hat_x;
-        Lambda                = Lambda_x;
+        hat_X  = hat_x;
+        Lambda = Lambda_x;
 
     case 'MFCC'
         
         % MMSE estimate is directly in complex domain
-        hat_X                 = hat_x;
-        Lambda                = Lambda_x;
+        hat_X  = hat_x;
+        Lambda = Lambda_x;
         
     case 'LSA'
         
         % Retrieve amplitude estimate, use mixture phase
-        hat_X                 = exp(hat_x).*alpha;
-        
-        % Patch zeros in mu_XcY with the Wiener estimate (zero). In
-        % fact this should be computed from the momments of a Rayleigh.
-        % but we have no phase estimate, so we set it to zero
-        hat_X(abs(mu_XcY)==0) = 0;
-        Lambda                = zeros(size(hat_X));
+        hat_X  = exp(hat_x).*alpha;
+        Lambda = zeros(size(hat_X));
         
     case 'STSA'
         
         % Retrieve amplitude estimate, use mixture phase
-        hat_X                 = hat_x.*alpha;
-        
-        % Patch zeros in mu_XcY with the Wiener estimate (zero). In
-        % fact this should be computed from the momments of a Rayleigh.
-        % but we have no phase estimate, so we set it to zero
-        hat_X(abs(mu_XcY)==0) = 0;
-        Lambda                = zeros(size(hat_X));
+        hat_X  = hat_x.*alpha;
+        Lambda = zeros(size(hat_X));
         
     case 'PSD'
         
         % Retrieve amplitude estimate, use mixture phase
-        hat_X                 = sqrt(hat_x).*alpha;
-        
-        % Patch zeros in mu_XcY with the Wiener estimate (zero). In
-        % fact this should be computed from the momments of a Rayleigh.
-        % but we have no phase estimate, so we set it to zero
-        hat_X(abs(mu_XcY)==0) = 0;
-        Lambda                = zeros(size(hat_X));
+        hat_X  = sqrt(hat_x).*alpha;
+        Lambda = zeros(size(hat_X));
         
 end
